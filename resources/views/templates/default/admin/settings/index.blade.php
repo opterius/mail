33720d09fcbedd30d034b366b9fdc7b7f09b4e47
Gabriel Chimilevschi <gabriel1978@gmail.com>
2026-04-11 14:44:15 +0300

    Update LICENSE and copyright headers
    - LICENSE: Host Server SRL (Opterius) copyright + Section 7 Opterius Agent exception
    - All file headers: @copyright Host Server SRL (Opterius), @author Iosif Gabriel Chimilevschi

--- resources/views/templates/default/admin/settings/index.blade.php
+++ resources/views/templates/default/admin/settings/index.blade.php
@@ -3,9 +3,9 @@
  | Modern, fast and responsive webmail that works with any IMAP/SMTP server.
  | https://opterius.com
  |
- | Copyright (c) Chimilevschi Iosif-Gabriel / Opterius
+ | Copyright (c) Host Server SRL (Opterius)
  | @license  AGPL-3.0  https://opensource.org/license/agpl-v3
- | @author   Chimilevschi Iosif-Gabriel <office@opterius.com>
+ | @author   Iosif Gabriel Chimilevschi <office@opterius.com>
 --}}
 @extends(mailView('admin.layouts.admin'))
 @section('title', 'Settings')
